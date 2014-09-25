7c3a0be675145b730705677dcaa1d7029dea72bb
Deaygo <deaygo@thezomg.com>
2014-09-25 16:02:05 -0400

    Switch to easily expand features, CRLF for writing and some organization

--- irc/protocol.py
+++ irc/protocol.py
@@ -1,5 +1,6 @@
 import asyncio
 import re
+from .numerics import get_type
 
 class IRCProtocol(asyncio.Protocol):
     """
@@ -20,15 +21,23 @@ class IRCProtocol(asyncio.Protocol):
         \Z''',
         flags=re.VERBOSE)
 
+    ###
+    # Events (must be overridden in implementor)
+    ###
+
     def connected(self):
         pass
 
     def on_message(self, sender, target, message):
         pass
 
+    ###
+    # Helper functions
+    ###
+
     def write(self, data):
         print("Sent: {}".format(data))
-        data = '{}\n'.format(data).encode('utf8')
+        data = '{}\r\n'.format(data).encode('utf8')
         self.transport.write(data)
 
     def nick(self, nickname):
@@ -43,27 +52,10 @@ class IRCProtocol(asyncio.Protocol):
     def join(self, channel):
         self.write('JOIN {}'.format(channel))
 
-    def connection_made(self, transport):
-        self.transport = transport
-        self.connected()
 
-    def connection_lost(self, exc):
-        pass
-
-    def data_received(self, data):
-        data = data.decode('utf8')
-        lines = data.split('\r\n')
-        for line in lines:
-            if len(line.strip()) == 0:
-                continue
-            print('Got: {}'.format(repr(line)))
-            command, args, prefix = self._parse(line)
-            if command == 'PING':
-                self.write('PONG :{}'.format(''.join(args)))
-            elif command == 'PRIVMSG':
-                target, message = args
-                sender = self._parse_user(prefix)
-                self.on_message(sender, target, message)
+    ###
+    # Utility functions (could be moved to a different file)
+    ###
 
     def _parse_user(self, hostmask):
         username, hostmask = hostmask.split('!')
@@ -85,6 +77,46 @@ class IRCProtocol(asyncio.Protocol):
             args.append(m.group('trailing'))
 
         return m.group('command'), args, m.group('prefix')
+
+    ###
+    # Handler functions
+    ###
+
+    def _handle_ping(self, prefix, args):
+        self.write('PONG :{}'.format(''.join(args)))
+
+    def _handle_privmsg(self, prefix, args):
+        target, message = args
+        sender = self._parse_user(prefix)
+        self.on_message(sender, target, message)
+
+
+
+    ###
+    # asyncio.Protocol functions
+    ###
+
+    def connection_made(self, transport):
+        self.transport = transport
+        self.connected()
+
+    def connection_lost(self, exc):
+        pass
+
+    def data_received(self, data):
+        data = data.decode('utf8')
+        lines = data.split('\r\n')
+        for line in lines:
+            if len(line.strip()) == 0:
+                continue
+            print('Got: {}'.format(repr(line)))
+            command, args, prefix = self._parse(line)
+
+            typ = get_type(command, args)
+
+            f = getattr(self, '_handle_{}'.format(typ.lower()), None)
+            if f != None:
+                f(prefix, args)
 
     def eof_received(self):
         self.transport.close()
